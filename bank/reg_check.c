b44b407e244548734a0f9e39bb1343b65955a39f
Jay Zhang <jayzhang@Jays-MacBook-Air.local>
2015-12-04 23:20:32 -0500

    system changed

---
+++ bank/reg_check.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stdbool.h>
+
+bool user_name_check (char *user_name) {
+	if (strlen(user_name) > 250)
+		return false;
+
+	int i;
+	for (i = 0; i < strlen(user_name); i++)
+		if ((int)user_name[i] < 65 
+			|| ((int)user_name[i] > 90 && (int)user_name < 97)
+			|| (int) user_name[i] > 122)
+			return false;
+
+	return true;
+}
+
+bool pin_check (char *pin) {
+	if (strlen(pin) != 4)
+		return false;
+
+	int i;
+	for (i = 0; i < strlen(pin); i++)
+		if ((int)pin[i] < 48 || (int)pin[i] > 57)
+			return false;
+
+	return true;
+}
